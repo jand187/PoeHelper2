2b7a89082e30e2ae4d17ec5e9813ab8d677b148d
Jan Daniel Anderse <jand187@gmail.com>
2015-01-02 13:21:46 +0100

    Added Item model.

---
+++ PoeHelper.Engine/Model/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PoeHelper.Engine.Model
+{
+	[DebuggerDisplay("{Rarity} {ItemType} {Name}")]
+	public class Item
+	{
+		public string Rarity { get; set; }
+		public string Name { get; set; }
+		public string ItemType { get; set; }
+		public IEnumerable<IItemMod> Mods { get; set; }
+	}
+}
